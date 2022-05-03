c0a621ae19ac96488a2ead756518e9963a1994ff
mgruppi <mauricio.gruppi@gmail.com>
2022-05-02 21:18:40 -0400

    Blog primer

---
+++ mgruppi.github.io/src/components/Blog.vue
@@ -0,0 +1,62 @@
+<script>
+import BlogEntry from '../components/BlogEntry.vue'
+export default {
+    setup() {
+        
+    },
+    components: {
+        BlogEntry: BlogEntry
+    },
+    data() {
+        return {
+            entries: [
+                {
+                    date: '2022-01-01',
+                    title: 'Latest Entry',
+                    content: 'Lorem Ipsum...',
+                    author: 'Maurício',
+                },
+                {
+                    date: '2021-12-01',
+                    title: 'First Entry',
+                    content: 'Lorem Ipsum Lorem...',
+                    author: 'Maurício',
+                }
+            ],
+            selected: 0,
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="d-flex mt-4">
+        <!-- LEFT COL -->
+
+        <!-- MIDDLE COL -->
+        <div class="col-9">
+            <div id="blogFeed" class="blog-feed">
+                <BlogEntry v-for="e in this.entries" :key="e" :entry="e" class="blog-entry"></BlogEntry>
+            </div>
+        </div>
+
+        <!-- RIGHT COL -->
+        <div class="col-3">
+            <div class="blog-archive">
+                <h5>Archive</h5>
+                <ul class="list-group">
+                    <li v-for="e in entries" :key="e" class="list-group-item list-group-item-dark"><div class="d-flex justify-content-between"><span>{{ e.title }}</span> <span>{{ e.date }}</span></div></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+
+.blog-feed .blog-entry{
+    margin-bottom: 8%;
+}
+
+</style>
---
+++ mgruppi.github.io/src/components/BlogEntry.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    setup() {
+        
+    },
+    data() {
+        return {
+        
+        }
+    },
+    props: {
+        entry: {required: true}
+    }
+}
+</script>
+
+<template>
+    <div class="px-2">
+        <h2> {{ this.entry.title }}</h2>
+        <div class="d-flex gap-2 mt-0"><div class="fw-bold"> {{ this.entry.date }}</div> <div class="badge bg-info text-dark" >{{ entry.author }} </div></div>
+        <p> {{ this.entry.content }} </p>
+    </div>
+</template>
+
+<style scoped>
+h2 {
+    margin-bottom: 0;
+}
+
+p {
+    margin-top: 1%;
+}
+</style>
