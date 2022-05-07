c5940a17c64387904a7ea66c67d87ace17e974b2
mgruppi <mauricio.gruppi@gmail.com>
2022-05-07 16:42:01 -0400

    Update about text

--- mgruppi.github.io/src/components/About.vue
+++ mgruppi.github.io/src/components/About.vue
@@ -10,23 +10,23 @@
 
             <div class="">
 
-            <p class="">
-            I am a PhD Candidate at Rensselaer Polytechnic Institute (since 2017).
-            My research interests include machine learning and natural language processing methods and applications,
-            particularly to misinformation, and semantic change and bias.
-            </p>
-            <p class="">
-            Currently, my research focus on methods for semantic change detection and language representation.
-            My goal is to find ways to understand and explain the divergence in language usage in different domains,
-            and how those can be used by humans and machines.
-            </p>
+                <p class="">
+                I am a PhD Candidate at Rensselaer Polytechnic Institute (since 2017) under the advisorship of professor <a href="https://www.cs.rpi.edu/~sibel/www/index.html">Sibel Adalı</a>.
+                My research interests include machine learning and natural language processing methods and applications,
+                particularly to misinformation, and semantic change and bias.
+                </p>
+                <p class="">
+                My research focus on methods and applications of semantic change detection and language representation.
+                My goal is to find ways to understand and explain the divergence in language usage in different domains,
+                and how those can be used by humans and machines. And also to explore how language shift can be used to uncover latent
+                differences across time and domain.
+                </p>
 
             <div class="col col-sm-auto justify-content-md-center" style="text-align: center;">
-            <img :src="'./me.jpg'" style="width: 200px; border-style: solid;">
+            <img :src="'./me.jpg'" style="width: 200px;">
 
-            <p class="">
-                Rensselaer Polytechnic Institute <br/>
-                Troy, NY, USA </p>
+            <p class="fst-italic fw-light">
+                Maurício in 2018, Curitiba, Brazil </p>
             </div>
             </div>
         </div>
